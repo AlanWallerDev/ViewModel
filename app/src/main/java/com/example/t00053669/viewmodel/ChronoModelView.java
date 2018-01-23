8d0ddffedbf4e46faf42f70eb09bf0197dbfbc86
Alan Waller <zancar615@gmail.com>
2018-01-22 19:05:37 -0800

    Stuff

--- app/src/main/java/com/example/t00053669/viewmodel/ChronoModelView.java
+++ app/src/main/java/com/example/t00053669/viewmodel/ChronoModelView.java
@@ -1,20 +1,38 @@
 package com.example.t00053669.viewmodel;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.os.SystemClock;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by t00053669 on 1/22/2018.
  */
 
 public class ChronoModelView extends ViewModel {
+    private MutableLiveData<Long> mStartTimer;
 
-    private Long mStartTimer;
-
-    public Long getmStartTimer() {
-        return mStartTimer;
+    public ChronoModelView() {
+        mStartTimer = new MutableLiveData<>();
+        final long currentTime = SystemClock.elapsedRealtime();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(
+                new TimerTask() {
+                    @Override
+                    public void run() {
+                        final long newTime = (SystemClock.elapsedRealtime() - currentTime / 1000);
+                        mStartTimer.postValue(newTime);
+                    }
+                }
+        , 1000, 1000);
     }
 
-    public void setmStartTimer(Long mStartTimer) {
-        this.mStartTimer = mStartTimer;
+
+
+    public LiveData<Long> getmStartTimer() {
+        return mStartTimer;
     }
 }
--- app/src/main/java/com/example/t00053669/viewmodel/MainActivity.java
+++ app/src/main/java/com/example/t00053669/viewmodel/MainActivity.java
@@ -1,29 +1,36 @@
 package com.example.t00053669.viewmodel;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ChronoModelView vm;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Chronometer chrono = (Chronometer) findViewById(R.id.chronometer1);
-
+        tv = (TextView) findViewById(R.id.textView);
         vm = ViewModelProviders.of(this).get(ChronoModelView.class);
-        if(vm.getmStartTimer() == null){
-            long starttime = SystemClock.elapsedRealtime();
-            vm.setmStartTimer(starttime);
-            chrono.setBase(starttime);
-        }else{
-            chrono.setBase(vm.getmStartTimer());
-        }
-        chrono.start();
+        subscribe();
+
+    }
+
+    private void subscribe(){
+        final Observer<Long> timeObserver = new Observer<Long>() {
+            @Override
+            public void onChanged(@Nullable Long aLong) {
+                tv.setText(aLong + "");
+            }
+        };
+        vm.getmStartTimer().observe(this, timeObserver);
     }
 }
